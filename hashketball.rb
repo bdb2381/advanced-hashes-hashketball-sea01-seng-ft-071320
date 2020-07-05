c365697d64b2358c40d36060f15caae6b951371d
Brad Beggs <brad.beggs@gmail.com>
2020-07-05 23:54:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -256,9 +256,9 @@ def big_shoe_rebounds
 
 # 1. Unify the data
 all_player_data = hash[:home][:players] + hash[:away][:players]
-puts all_player_data
+#puts all_player_data
 # 2. Extract the data you want
-all_shoe_sizes_and_rebounds = all_player_data.map { |_id, player_data| [player_data[:shoe_size], player_data[:rebounds]] }
+all_shoe_sizes_and_rebounds = all_player_data.map { |_id, player_data| [all_player_data[:shoe_size], all_player_data[:rebounds]] }
 puts all_shoe_sizes_and_rebounds
 # 3. Filter the data
 max_shoe_size_and_rebounds = all_shoe_sizes_and_rebounds.max_by(&:first)
