8b75984a9e9f1d985c7fa214cdb38cfa266403f2
Brad Beggs <brad.beggs@gmail.com>
2020-07-06 00:18:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -257,8 +257,10 @@ def big_shoe_rebounds
 all_player_data = hash[:home][:players] + hash[:away][:players]
 #print all_player_data
 
-all_player_data.max do |first_player, second_player|
+test = all_player_data.max do |first_player, second_player|
   first_player[:shoe] <=> second_player[:shoe]
+end 
+
 
  Filter the data
 max_shoe_size_and_rebounds = all_shoe_sizes_and_rebounds.max_by(&:first)
