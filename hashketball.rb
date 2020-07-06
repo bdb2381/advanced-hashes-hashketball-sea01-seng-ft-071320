5f235fd852683d9891d0050a411f3b0b1a0fa511
Brad Beggs <brad.beggs@gmail.com>
2020-07-06 03:13:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -167,6 +167,7 @@ def shoe_size(player_name)
     player_stats.each do |name|
       if name[:player_name] == player_name
         return name[:shoe]
+        binding.pry
       end #end if
 
     end #end player_stats do loop
