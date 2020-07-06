77e85608ce5241923ea5897d7d66573f3a414cf3
Brad Beggs <brad.beggs@gmail.com>
2020-07-06 00:54:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -240,7 +240,7 @@ def big_shoe_rebounds
   all_player_data = hash[:home][:players] + hash[:away][:players]   #Combine the two team hashes into one array of hashes  to make it easier to compare
 
   #loop through with max, looking to see which :shoe is bigger, based on 1, 0, -1, max returns the hash of the largest shoe
-  player_with_largest_shoe = all_player_data.max_by do |first_player, second_player|
+  player_with_largest_shoe = all_player_data.max do |first_player, second_player|
     first_player[:shoe] <=> second_player[:shoe]
   end
 
